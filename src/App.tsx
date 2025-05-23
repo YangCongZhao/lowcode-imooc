c62b592430aa5d09c2c36150f12968062520b531
DESKTOP-9N45CRK\Yangc <15315575962@163.com>
2025-05-23 09:12:54 +0800

    fix(bug1111): sdsdsd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import type { MouseEvent } from 'react'
 import 'antd/dist/reset.css'
 function App() {
+  function handleClick(event: MouseEvent<HTMLButtonElement>) {
+    console.log(event)
+  }
+  type qlistType = {
+    id: string
+    label: string
+    isPublish: boolean
+  }
+
+  const [qlist] = React.useState<qlistType[]>([
+    {
+      id: 'q1',
+      label: '问卷1',
+      isPublish: true,
+    },
+    {
+      id: 'q2',
+      label: '问卷2',
+      isPublish: false,
+    },
+    {
+      id: 'q3',
+      label: '问卷3',
+      isPublish: false,
+    },
+    {
+      id: 'q4',
+      label: '问卷4',
+      isPublish: false,
+    },
+  ])
+
   return (
     <>
-      <h1>App page</h1>
+      <h1>问卷列表页</h1>
+      {qlist.map(item => (
+        <div key={item.id}>
+          <span>{item.label}</span>
+          <span>
+            {item.isPublish ? <span style={{ color: 'green' }}>已发布</span> : <span>未发布</span>}
+          </span>
+          <button onClick={handleClick}>编辑</button>
+        </div>
+      ))}
     </>
   )
 }
